c6923c0d14e59d5e796b9c6c8211dae8ddbfd71e
bhaveshpadave <bhaveshpadave97@gmail.com>
2024-07-05 14:53:22 +0530

    Add models classes

---
+++ src/main/java/com/scaler/productservice/models/Product.java
@@ -0,0 +1,14 @@
+package com.scaler.productservice.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product extends BaseModel {
+    private String title;
+    private String description;
+    private int quantity;
+    private double price;
+    private Category category;
+}
