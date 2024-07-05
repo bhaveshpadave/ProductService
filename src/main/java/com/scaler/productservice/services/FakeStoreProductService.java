f7c44d20d5a3668afdf1a3eac81b23befcfeaa28
bhaveshpadave <bhaveshpadave97@gmail.com>
2024-07-05 15:41:11 +0530

    Update sevices to add getAllProducts API

--- src/main/java/com/scaler/productservice/services/FakeStoreProductService.java
+++ src/main/java/com/scaler/productservice/services/FakeStoreProductService.java
@@ -7,6 +7,7 @@ import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -30,7 +31,16 @@ public class FakeStoreProductService implements ProductService {
 
     @Override
     public List<Product> getAllProducts() {
-        return List.of();
+        FakeStoreProductDto[] fakeStoreProductDtos = restTemplate.getForObject(
+                "https://fakestoreapi.com/products",
+                FakeStoreProductDto[].class);
+
+        List<Product> products = new ArrayList<>();
+        for(FakeStoreProductDto fakeStoreProductDto : fakeStoreProductDtos){
+            products.add(convertFakeStoreProductToProduct(fakeStoreProductDto));
+        }
+
+        return products;
     }
 
     private Product convertFakeStoreProductToProduct (FakeStoreProductDto fakeStoreProductDto){
